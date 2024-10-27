398923bdb95185ea607a42e243b65da23529a623
ghelaseG <ghelase.georgian@gmail.com>
2024-10-27 22:00:12 +0000

    my trial with dataset from kaggle

--- Creating a ggplot/1.R
+++ Creating a ggplot/1.R
@@ -6,4 +6,21 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy))
 
 
+# install.packages("psych")
+# install.packages("knitr")
+# second example
+# load data to a dataframe called fifa teams
+library(psych)
+library(knitr)
 
+
+fifa_teams <- read_csv("fifa_teams_cleaned.csv")
+View(fifa_teams)
+summary(fifa_teams)
+describe(fifa_teams) %%
+  select("Mean" = "mean", "SD" = "sd", "Median" = "median", "Min" = "min", "Max" = "max")
+  kable(digits = 1)
+
+
+ggplot(data = fifa_teams) +
+  geom_boxplot(mapping = aes(x =))
