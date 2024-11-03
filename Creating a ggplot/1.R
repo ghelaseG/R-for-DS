7fb347b52a57aa73c1425bd38d1bb23badf1597c
ghelaseG <ghelase.georgian@gmail.com>
2024-11-03 12:58:04 +0000

    exercises

--- Creating a ggplot/1.R
+++ Creating a ggplot/1.R
@@ -25,4 +25,39 @@ table(fifa_teams$`Whole team average age`, fifa_teams$date)
 ggplot(data = fifa_teams) +
   geom_point(mapping = aes(x = 'Whole team average age', y = date))
 
+# Exercises
+#1. Run ggplot(data = mpg). What do you see?
+ggplot(data = mpg)
+#Answer: empty screen
+
+#2. How many rows are in mtcars? How many columns?
+nrow(mtcars)
+ncol(mtcars)
+#Answer: 32 rows, 11 columns
+
+#3. What does the drv variable describe? Read the help for ?mpg to find out.
+?mpg
+#Answer:drv - the type of drive train, where f = front-wheel drive, r = rear wheel drive, 4 = 4wd
+
+#4. Make a scatterplot of hwy versus cyl.
+#Notes: hwy - highway miles per gallon
+#       cyl - number of cylinders
+  
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = cyl, y = hwy))
+
+#5. What happens if you make a scatterplot of class versus drv? Why is the plot not useful?
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = class, y = drv))
+#Answer: scatterplot does not suit our chosen data.
+#        important trends hidden.
+
+
+
+
+
+
+
+
 
