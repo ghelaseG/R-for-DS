31fe820f5be703511ffe724313ba141727ffa02a
ghelaseG <ghelase.georgian@gmail.com>
2024-11-23 23:48:45 +0000

    exercise 6, part 6

--- Creating a ggplot/1.R
+++ Creating a ggplot/1.R
@@ -450,5 +450,8 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy, linetype = drv)) +
   geom_point(mapping = aes(x = displ, y = hwy, colour = drv,)) +
   geom_smooth()
 
+#3rd row right
 
-
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point(mapping = aes(x = displ, y = hwy, colour = drv)) 
+  
