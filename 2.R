d69d22df7daa3e9ac087f759e8fc270e844d14c9
ghelaseG <ghelase.georgian@gmail.com>
2024-12-02 22:43:12 +0000

    update

---
+++ 2.R
@@ -0,0 +1,39 @@
+# CHAPTER 2
+# Workflow: Basics
+
+library(tidyverse)
+
+# Coding Basics
+
+1 / 200 * 30
+
+(59 + 73 + 2) / 3
+
+sin(pi / 2)
+
+x <- 3 * 4 #same as object_name <- value ("object name gets value")
+
+x
+
+#more example
+
+this_is_a_really_long_name <- 3.5
+this_is_a_really_long_name
+
+r_rocks <- 2 ^ 3
+r_rock
+
+
+# Calling Functions
+
+seq(1, 10)
+
+x <- "hello world"
+
+x <- "hello
+
+y <- seq(1, 10, length.out = 5)
+
+y
+
+(y <- seq(1, 10, length.out = 5))
