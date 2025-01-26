be45bc7e71fa2c0fff253c683caf19b581bfe65b
ghelaseG <ghelase.georgian@gmail.com>
2025-01-26 12:43:42 +0000

    CHAPTER 5, EDA: introduction

---
+++ Chapter 5/5.R
@@ -0,0 +1,18 @@
+# EXPLORATORY DATA ANALYSIS
+
+
+
+#INTRODUCTION
+
+#here we are going to use visualisation and transofrmation to explore our data in a systematic way, a task that statisticians call EDA
+
+#Few steps to follow:
+
+#1. Generate questions about your data
+#2. Search for answers by visualising, transofrimng, and modeling your data
+#3. Use what you learn to refine your questions and/or generate new questions
+
+#EDA is not a process with a strict set of rules, is more a state of mind.
+#when starting EDA, you should feel free to investigate every idea that occurs to you
+#data cleaning is just one applications of EDA, to do data cleaning you'll need to deploy all the tools of EDA: visualisation, transformation and modeling.
+
