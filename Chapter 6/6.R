cf8239eb6431d57a0f04918322a1a9e03571362d
ghelaseG <ghelase.georgian@gmail.com>
2025-02-23 11:59:03 +0000

    chapter 6 - what is real

--- Chapter 6/6.R
+++ Chapter 6/6.R
@@ -2,3 +2,7 @@
 
 # WORKFLOW: PROJECTS
 
+#there is a great pair of shortcuts that will work together to make sure you've captured the important parts of your code in the editor:
+
+#Press Cmd/Ctrl-Shift-F10 to restart R Studio.
+#Press Cmd/Ctrl-Shift-S to rerun the current script.
