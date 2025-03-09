93b175fb1a5213ff7cec709254715d11554c36b5
ghelaseG <ghelase.georgian@gmail.com>
2025-03-09 23:58:37 +0000

    chapter 9, tidy data with tidyr

---
+++ Part 2 WRANGLE/Chapter 9/9.R
@@ -0,0 +1,7 @@
+# TIDY DATA WITH TIDYR
+
+# this chapter presents a consisten way to organise your data in R.
+
+#this work pays in a long term
+
+#the Tidy Data paper at https://www.jstatsoft.org/article/view/v059i10 will give you more underlying theory
