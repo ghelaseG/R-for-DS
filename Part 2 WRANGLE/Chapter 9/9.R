d934c9abfaa729fb8860d06798ebe2d90c0269f7
ghelaseG <ghelase.georgian@gmail.com>
2025-03-13 22:56:07 +0000

    useful videos

--- Part 2 WRANGLE/Chapter 9/9.R
+++ Part 2 WRANGLE/Chapter 9/9.R
@@ -5,3 +5,6 @@
 #this work pays in a long term
 
 #the Tidy Data paper at https://www.jstatsoft.org/article/view/v059i10 will give you more underlying theory
+
+# useful videos: https://www.youtube.com/watch?v=xrrWLKRfZWY&list=PLu6UwBFCnlEc7m2aK-AGfLLM-auJ5K3i5&ab_channel=StatisticsGlobe and https://www.youtube.com/watch?v=pLg-Bf_ppSU&ab_channel=DataWithinReach
+
