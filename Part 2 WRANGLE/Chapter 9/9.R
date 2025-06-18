9e41b74364deb35923ab52744d7c2ab902d81bb0
ghelaseG <ghelase.georgian@gmail.com>
2025-06-18 23:03:18 +0100

    tidy data with tidyr, spreading and gathering, exercise 1

--- Part 2 WRANGLE/Chapter 9/9.R
+++ Part 2 WRANGLE/Chapter 9/9.R
@@ -122,3 +122,18 @@ left_join(tidy4a, tidy4b)
 spread(table2, key = type, value = count)
 
 table2
+
+# Exercises
+
+#1. Why are gather() and spread() not perfectly symmetrical? Carefully consider the following example:
+
+stocks <- tibble(
+  year = c(2015, 2015, 2016, 2016),
+  half = c(1,    2,     1,     2),
+  return = c(1.88, 0.59, 0.92, 0.17))
+stocks %>% 
+  spread(year, return) %>%
+  gather("year", "return", `2015`:`2016`)
+
+
+)
