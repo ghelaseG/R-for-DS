1259e318293108d3a114c54f67042a375c41ac3d
ghelaseG <ghelase.georgian@gmail.com>
2025-07-05 23:58:09 +0100

    stringr, notes

---
+++ Part 2 WRANGLE/Chapter 11/11.R
@@ -0,0 +1,17 @@
+#CHAPTER 11
+#STRINGS WITH stringr
+
+#here we talk about string manipulation
+
+#focus will be on regexps - regular expressions
+
+library(tidyverse)
+library(stringr)
+
+"This is a string without a closing quote"
+
+double_quote <- "\"" #or '"'
+single_quote <- '\'' #or "'"
+
+#to see raw contents of the strings, use writeLines()
+
