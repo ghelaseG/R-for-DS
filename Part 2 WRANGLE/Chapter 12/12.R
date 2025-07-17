e9f3fc1e0b9998e4513b02d3a11011cb729fdfc3
ghelaseG <ghelase.georgian@gmail.com>
2025-07-17 22:02:20 +0100

    chapter 12 and notes

--- Part 2 WRANGLE/Chapter 12/12.R
+++ Part 2 WRANGLE/Chapter 12/12.R
@@ -1,2 +1,16 @@
 ### FACTORS with forcats
 
+#factors are used to work with categorical variables
+#useful when you want to display character vectors in a non-alphabetical order
+
+#historically, factors were much easier to work with than characters
+
+#good websites:
+
+## https://simplystatistics.org/posts/2015-07-24-stringsasfactors-an-unauthorized-biography/
+## https://notstatschat.tumblr.com/post/124987394001/stringsasfactors-sigh
+
+
+library(tidyverse)
+library(forcats)
+
