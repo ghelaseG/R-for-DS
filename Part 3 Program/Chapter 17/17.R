8f8bf2110ea3cf18c2703febb1d29373bd543d90
ghelaseG <ghelase.georgian@gmail.com>
2025-08-05 23:56:07 +0100

    part 3 -chapter 17, iteration with purrr, for loops, exercise 3, c, trial

--- Part 3 Program/Chapter 17/17.R
+++ Part 3 Program/Chapter 17/17.R
@@ -293,3 +293,40 @@ for (i in output) {
 }
 
 #c)
+#99 Bottles of Beer on the Wall Lyrics
+"""
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+98 bottles of beer on the wall, 98 bottles of beer.
+Take one down and pass it around, 97 bottles of beer on the wall.
+
+97 bottles of beer on the wall, 97 bottles of beer.
+Take one down and pass it around, 96 bottles of beer on the wall.
+
+96 bottles of beer on the wall, 96 bottles of beer.
+Take one down and pass it around, 95 bottles of beer on the wall.
+
+.... ...... ......
+
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+"""
+
+output <- c(1:99)
+
+for (i in output) {
+  str_c(i, "bottles of beer on the wall,", i, "bottles of beer.\n")
+  cat("Take one down and pass it around,", i, "bottles of beer on the wall.\n")
+  
+}
+
