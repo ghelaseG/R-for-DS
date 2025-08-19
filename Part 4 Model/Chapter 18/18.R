17c4440db41ab911bd1ecd26944ae7ff6b220085
ghelaseG <ghelase.georgian@gmail.com>
2025-08-19 22:26:30 +0100

    part 4 - model, chapter 18: modelr, notes

---
+++ Part 4 Model/Chapter 18/18.R
@@ -0,0 +1,7 @@
+# Model Basics with modelr
+
+#we are going to use models to partition data into patterns and residuals(residuals represent the difference between observed values and predicted values)
+#source Google: Example: If a regression model predicts a student's test score to be 85, and the actual score is 90, the residual is 5. This indicates the model underestimated the score by 5 points.
+
+#strong patterns will hide subtler trends.
+
---
+++ Part 4 Model/notes modeling.R
@@ -0,0 +1,28 @@
+"""
+Part IV: MODEL
+
+In the next chapters we use the tools of data wrangling and programming to fit models and see how they work.
+
+The goal of a model is to provide a simple low-dimensional summary of a dataset.
+
+These models help you discover interesting relationships within your data.
+
+In the following chapter, we are given a family of useful tools that allow us to use models to understand data.
+
+"""
+
+"""
+Hypothesis generation vs Hypothesis confirmation
+
+We are using models as a tool for exploration.
+
+The focus of modeling is on inference (or confirming is true):
+- observation can be used as many times for exploration or once for confirmation.
+
+Exploratory analysis can never be confirmatory analysis.
+
+Data can be split into 3 pieces:
+- 60% goes into training
+- 20% goes into query set (compare models by hand)
+- 20% held for test set (use it once to test the final model, but still need to do eda) 
+"""
