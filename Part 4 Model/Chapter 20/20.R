3d9fcf317ec10574d0f6aec987f9335e768d32d2
ghelaseG <ghelase.georgian@gmail.com>
2025-09-14 23:31:25 +0100

    part 4 - many models with purrr and broom, chapter 20, gapminder notes

--- Part 4 Model/Chapter 20/20.R
+++ Part 4 Model/Chapter 20/20.R
@@ -1,3 +1,23 @@
 # Chapter 20
 
 # Many Models with purrr and broom
+
+#here we learn how to use many simple models to better understand complex datasets, use list-columns to store arbitrary data structures in a data frame and use the broom package, to turn models into tidy data.
+
+#this chapter is a bit more complex and requires a good understanding about modeling, data structures and iteration.
+
+library(modelr)
+library(tidyverse)
+
+#gapminder
+
+#this data was made by a Swedish doctor and statistician - Hans Rosling.
+
+library(gapminder)
+gapminder %>% view()
+
+# "How does life expectancy (lifeExp) change over time (year) for each country (country)?"
+
+gapminder %>% 
+  ggplot(aes(year, lifeExp, group = country)) +
+  geom_line(alpha = 1/3)
