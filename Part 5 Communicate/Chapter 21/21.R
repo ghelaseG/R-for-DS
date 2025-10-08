57e437b0fba3198587ea38b989a60f2a2fbd6912
ghelaseG <ghelase.georgian@gmail.com>
2025-10-08 21:45:41 +0100

    part 5 - communicate, chapter 21, r markdown, notes

---
+++ Part 5 Communicate/Chapter 21/21.R
@@ -0,0 +1,9 @@
+# R Markdown
+
+# combines your code with the results and the commentary; this supports PDFs, Word files etc.
+
+#these files are good of use in communication, collaboration and as a modern lab notebook.
+
+#some useful resources mentioned in the book:
+
+## R Markdown Cheat Sheet and the Reference Guide.
