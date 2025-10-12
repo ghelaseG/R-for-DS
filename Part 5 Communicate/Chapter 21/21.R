80b3bd565e2efbaf37a46801ddce5c0c0f7ea9b6
ghelaseG <ghelase.georgian@gmail.com>
2025-10-12 22:49:36 +0100

    part 5 - communicate, chapter 21, r markdown, exercise 2

--- Part 5 Communicate/Chapter 21/21.R
+++ Part 5 Communicate/Chapter 21/21.R
@@ -17,4 +17,4 @@
 
 #3. Compare and contrast the R Notebook and R Markdown files you created earlier. How are the outputs similar? How are they different? How are the inputs similar? How are they different? What happens if you copy the YAML header from one to the other?
 
-#4. Create one new R Markdown document for each of the three built-in formats: HTML, PDF, and Word. Knit each of the three documents. How does the output differ? How does the input differ? (You may need to install LaTeX in order to build the PDF output - RStudio will prompt you if the is necessary.)
+#4. Create one new R Markdown document for each of the three built-in formats: HTML, PDF, and Word. Knit each of the three documents. How does the output differ? How does the input differ? (You may need to install LaTeX in order to build the PDF output - RStudio will prompt you if this is necessary.)
