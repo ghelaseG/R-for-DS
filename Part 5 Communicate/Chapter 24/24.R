77b0d2f8a213f3b88ebfc4787f00fb71c7021b30
ghelaseG <ghelase.georgian@gmail.com>
2025-11-02 12:05:21 +0000

    part 5 - communicate, chapter 24, r markdown workflow, THE END

---
+++ Part 5 Communicate/Chapter 24/24.R
@@ -0,0 +1,5 @@
+#R Markdown Workflow
+
+#some great ideas:
+#use the date in the YAML file when you start writing your report.
+#if you come back to your code after some time, better to use packrat - stores the packages or checkpoint - reinstall the available packages
